0dfc31fed5eb165593ce5878024c3d2a7547ed2c
Your Name <yourmail@example.com>
2026-02-07 21:32:12 +0530

    1.14 updated

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -7,14 +7,12 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
 
   const [selected, setSelected] = useState(0)
-
-  // create a zero-filled votes array
   const [votes, setVotes] = useState(
     Array(anecdotes.length).fill(0)
   )
@@ -25,18 +23,29 @@ const App = () => {
   }
 
   const voteAnecdote = () => {
-    const copy = [...votes]   // make a copy
-    copy[selected] += 1       // increment vote
+    const copy = [...votes]
+    copy[selected] += 1
     setVotes(copy)
   }
 
+  // find index of anecdote with most votes
+  const maxVotes = Math.max(...votes)
+  const mostVotedIndex = votes.indexOf(maxVotes)
+
   return (
     <div>
+      <h2>Anecdote of the day</h2>
+
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
 
       <button onClick={voteAnecdote}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
+
+      <h2>Anecdote with most votes</h2>
+
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>has {votes[mostVotedIndex]} votes</p>
     </div>
   )
 }
