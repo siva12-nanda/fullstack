18d7b97496fb809c4c0cb6e0f838d1ea188a5b0f
Your Name <yourmail@example.com>
2026-02-07 21:27:32 +0530

    1.2 updated

--- part1/anecdote/src/App.jsx
+++ part1/anecdote/src/App.jsx
@@ -7,21 +7,35 @@ const App = () => {
     'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Debugging is twice as hard as writing the code in the first place.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
 
   const [selected, setSelected] = useState(0)
 
+  // create a zero-filled votes array
+  const [votes, setVotes] = useState(
+    Array(anecdotes.length).fill(0)
+  )
+
   const nextAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+  const voteAnecdote = () => {
+    const copy = [...votes]   // make a copy
+    copy[selected] += 1       // increment vote
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+
+      <button onClick={voteAnecdote}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
     </div>
   )
