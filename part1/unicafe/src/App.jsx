f235562ab4b855a866aba2bdb2199e57f804f76e
Your Name <yourmail@example.com>
2026-02-07 11:20:26 +0530

    1.11 updated

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,10 @@ const Button = ({ text, onClick }) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -19,14 +22,16 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = (good / all) * 100
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + ' %'} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
