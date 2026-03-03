6b416102dfb52ed580d98c9980d8cf93ec3a23fe
Your Name <yourmail@example.com>
2026-03-03 06:30:21 +0530

    2.20 modified

---
+++ part2/countries/src/services/weather.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const api_key = import.meta.env.VITE_SOME_KEY
+
+const getWeather = (lat, lon) => {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    )
+    .then(response => response.data)
+}
+
+export default { getWeather }
