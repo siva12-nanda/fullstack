adb219a19ed6db4367feccddd2ac0e9551228fb3
Your Name <yourmail@example.com>
2026-03-08 14:37:17 +0530

    3.15

--- fullstackk/phonebook_backend/index.js
+++ fullstackk/phonebook_backend/index.js
@@ -2,83 +2,85 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Person = require('./models/person') // Import Mongoose model
+const Person = require('./models/person')
 
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
-// Morgan token to log request body
+// Morgan logging
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// ------------------ ROUTES ------------------
-
 // GET all persons
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    res.json(persons)
+    response.json(persons)
   })
 })
 
 // GET single person
-app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
     .then(person => {
       if (person) {
-        res.json(person)
+        response.json(person)
       } else {
-        res.status(404).end()
+        response.status(404).end()
       }
     })
     .catch(error => next(error))
 })
 
-// POST create new person
-app.post('/api/persons', (req, res, next) => {
-  const { name, number } = req.body
+// ADD new person
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-  if (!name || !number) {
-    return res.status(400).json({ error: 'name or number missing' })
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
   }
 
-  // Check if name already exists in DB
-  Person.findOne({ name })
-    .then(existingPerson => {
-      if (existingPerson) {
-        return res.status(400).json({ error: 'name must be unique' })
-      }
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
 
-      const person = new Person({ name, number })
-      return person.save()
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
     })
-    .then(savedPerson => res.json(savedPerson))
     .catch(error => next(error))
 })
 
 // DELETE person
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(() => res.status(204).end())
-    .catch(error => next(error))
-})
-
-// INFO route
-app.get('/info', (req, res, next) => {
-  Person.countDocuments({})
-    .then(count => {
-      res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-// ------------------ ERROR HANDLER ------------------
-const errorHandler = (error, req, res, next) => {
+// INFO route
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count => {
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
+       <p>${new Date()}</p>`
+    )
+  })
+})
+
+// ERROR HANDLER
+const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   next(error)
@@ -86,7 +88,6 @@ const errorHandler = (error, req, res, next) => {
 
 app.use(errorHandler)
 
-// ------------------ SERVER ------------------
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
